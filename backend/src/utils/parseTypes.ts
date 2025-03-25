7d6174571fd5d8f98391f74f5ee1baa5bf8ad696
Brendan Price <56489848+dev-brendanprice@users.noreply.github.com>
2025-03-25 02:43:50 +0000

    Rewrite backend in Typescript
    i did that!

---
+++ backend/src/utils/parseTypes.ts
@@ -0,0 +1,17 @@
+export default function parseTypes(types: String): Array<string> {
+    if (!types) return ['all']; // return all if none specified
+
+    let returnVal: Array<string> = (<string>types)?.split(',');
+
+    // change all items to lowercase
+    returnVal = returnVal.map((item) => {
+        return item.toLowerCase();
+    });
+
+    // remove array duplicates
+    returnVal = returnVal.filter((item, index) => {
+        return returnVal.indexOf(item) === index;
+    });
+
+    return returnVal;
+}
